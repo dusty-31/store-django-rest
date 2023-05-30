3f7539ad4cf2c3e3107d8dbaaa04db6f4c2faf27
Yuriy Rybak <jura.ryback007@gmail.com>
2023-05-30 23:21:08 +0300

    Created user types and signals for creating profiles

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User, Customer, Seller
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Seller)
+class SellerAdmin(admin.ModelAdmin):
+    pass
--- users/models.py
+++ users/models.py
@@ -1,10 +1,74 @@
+from django.contrib.auth.base_user import BaseUserManager
 from django.db import models
-from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.contrib.auth.models import AbstractUser
+from django.dispatch import receiver
 
 
-class UserProfile(models.Model):
-    user = models.OneToOneField(to=User, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='profile_images', blank=True)
+class User(AbstractUser):
+    class Role(models.TextChoices):
+        ADMIN = 'ADMIN', 'Admin'
+        CUSTOMER = 'CUSTOMER', 'Customer'
+        SELLER = 'SELLER', 'Seller'
+
+    base_role = Role.ADMIN
+
+    role = models.CharField(max_length=50, choices=Role.choices)
 
     def __str__(self):
-        return self.user.username
+        return f'{self.role} - {self.username}'
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.role = self.base_role
+            return super().save(*args, **kwargs)
+
+
+class CustomerManager(BaseUserManager):
+    def get_queryset(self, *args, **kwargs):
+        results = super().get_queryset(*args, **kwargs)
+        return results.filter(role=User.Role.CUSTOMER)
+
+
+class Customer(User):
+    base_role = User.Role.CUSTOMER
+    customers = CustomerManager()
+
+    class Meta:
+        proxy = True
+
+
+class CustomerProfile(models.Model):
+    user = models.OneToOneField(to=Customer, on_delete=models.CASCADE)
+    customer_id = models.IntegerField(null=True, blank=True)
+
+
+@receiver(signal=post_save, sender=Customer)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created and instance.role == 'CUSTOMER':
+        CustomerProfile.objects.create(user=instance)
+
+
+class SellerManager(BaseUserManager):
+    def get_queryset(self, *args, **kwargs):
+        results = super().get_queryset(*args, **kwargs)
+        return results.filter(role=User.Role.SELLER)
+
+
+class Seller(User):
+    base_role = User.Role.SELLER
+    sellers = SellerManager()
+
+    class Meta:
+        proxy = True
+
+
+class SellerProfile(models.Model):
+    user = models.OneToOneField(to=Seller, on_delete=models.CASCADE)
+    seller_id = models.IntegerField(null=True, blank=True)
+
+
+@receiver(signal=post_save, sender=Seller)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created and instance.role == 'SELLER':
+        SellerProfile.objects.create(user=instance)
