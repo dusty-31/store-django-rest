1c681d02b411b3a343933ad540f65e5d1dca94ff
Yuriy Rybak <jura.ryback007@gmail.com>
2023-05-30 23:14:52 +0300

    Updated models in products and created new migrations

--- products/models.py
+++ products/models.py
@@ -1,9 +1,17 @@
 from django.db import models
+from django.db.models.signals import pre_delete
+from django.dispatch import receiver
+
+from users.models import Seller
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=55, unique=True)
+    name = models.CharField(max_length=40, unique=True)
     description = models.TextField()
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
 
     def __str__(self):
         return self.name
@@ -11,23 +19,36 @@ class Category(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=50)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.ForeignKey(to=Category, on_delete=models.PROTECT)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(to=Seller, on_delete=models.PROTECT)
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
 
     def __str__(self):
-        return f"Product: {self.name} | Category: {self.category.name}"
+        return f"ID : {self.id} | Product: {self.name} | Category: {str(self.category)} | Price: {self.price} $"
+
+
+@receiver(signal=pre_delete, sender=Seller)
+def set_product_inactive(sender, instance, **kwargs):
+    instance.is_active = False
 
 
 class ProductDetail(models.Model):
     product = models.OneToOneField(to=Product, on_delete=models.PROTECT)
     description = models.TextField()
-    quantity = models.PositiveIntegerField(default=0, null=True, blank=True)
+    quantity = models.PositiveIntegerField(default=0)
     resolution = models.CharField(max_length=15, null=True, blank=True)
     processor = models.CharField(max_length=40, null=True, blank=True)
     storage = models.CharField(max_length=50, null=True, blank=True)
     operating_system = models.CharField(max_length=20, null=True, blank=True)
 
+    class Meta:
+        verbose_name = 'Product detail'
+
     def __str__(self):
-        return f"Product: {self.product.name} | Category: {self.product.category.name}"
+        return str(self.product)
